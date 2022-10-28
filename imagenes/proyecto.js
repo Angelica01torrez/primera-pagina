10c7bc681d9b5a5860e0745973659029b7b86c24
Angelica01torrez <104935418+Angelica01torrez@users.noreply.github.com>
2022-10-28 13:03:10 -0400

    Add files via upload

---
+++ imagenes/proyecto.js
@@ -0,0 +1,6 @@
+function multi(){
+    let n1=document.getElementById('n1').value;
+    let n2=document.getElementById('n2').value;
+    let edad=n2-n1;
+    document.getElementById("RESULTADO").innerHTML=" Usted nacio el: " +edad;  
+}
